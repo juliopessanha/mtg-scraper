c1c87e00df8115ffc4e169deb82e80841096170d
juliopessanha <juliomnp@hotmail.com>
2021-11-10 11:33:05 -0300

    Fixing possible robots.txt conflict The calling command in "run.py" now explicitly defines '--set="ROBOTSTXT_OBEY=True"' to avoid conflict

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ import os
 
 if __name__ == "__main__":
 	folder_path = os.path.abspath("./")
-	command = ('scrapy runspider mtg_crawler.py -O %s/cards.json' % (folder_path))
+	command = ('scrapy runspider mtg_crawler.py -O %s/cards.json --set="ROBOTSTXT_OBEY=True"' % (folder_path))
 	os.system(command)
